0cf5c136cbb88eb280556c2d872cf8181a422719
Christopher Danan <chrisddan@gmail.com>
2015-08-09 17:56:13 -0700

    Add controls to determine type of dice thrown and some script to determine if input text boxes are disabled or not.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,8 +3,24 @@ var main = function(){
 
 	console.log("Let's roll some dice, Vane!");
 
+	$("#normalDice").on("click", function(){
+		console.log("Regular Dice clicked");
+
+		$("#startNum").prop("disabled", true);
+		$("#endNum").prop("disabled", true);
+	});
+
+	$("#numberedDice").on("click", function(){
+		console.log("Numbered Dice clicked");
+
+		$("#startNum").prop("disabled", false);
+		$("#endNum").prop("disabled", false);
+	});
+
 	$("#roll-btn").on("click", function(){
 		console.log("You rolled the dice.");
+
+
 	});
 };
 
