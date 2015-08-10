5da428b11da246037d083520997067793ad06273
Christopher Danan <chrisddan@gmail.com>
2015-08-09 17:08:44 -0700

    Add div elements and a button that will be used to roll the dice.

--- javascripts/app.js
+++ javascripts/app.js
@@ -3,7 +3,9 @@ var main = function(){
 
 	console.log("Let's roll some dice, Vane!");
 
-	
+	$("#roll-btn").on("click", function(){
+		console.log("You rolled the dice.");
+	});
 };
 
 $(document).ready(main);
